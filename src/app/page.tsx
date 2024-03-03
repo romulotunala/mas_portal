0c0f7c472b3d4e79f74dbdd066cb458ca80af817
Rômulo Tunala <romulot@ciandt.com>
2024-03-02 23:17:21 -0300

    Create a Comming Soon page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,93 +3,32 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
+    <>
+      <main className={styles.container}>
         <Image
           className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
+          src="/logo.jpeg"
+          alt="Logo Ministério Anunciando Salvação"
+          width={100}
+          height={100}
           priority
         />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <h1>Ministério Anunciando Salvação</h1>
+        <h2>Site em construção</h2>
+        <p>
+          Somos uma igreja que entende, à luz da Bíblia, <br /> 
+          que o amor, a graça e a salvação se estendem a todos.
+        </p>
+        <p>Em breve estaremos com um novo site repleto de novidades!</p>
+        <p className={styles.address}><span>Endereço:</span> Rua Projetada, 18 - Piabeta - Magé - RJ</p>
+      </main>
+      <footer className={styles.footer}>
+        <ul className={styles.socialLinks}>
+          <li><a href="https://www.instagram.com/mas.sede" target="_blank">instagram</a></li>
+          <li><a href="https://www.facebook.com/ministerioanunciandosalvacao" target="_blank">facebook</a></li>
+          <li><a href="https://www.youtube.com/@mas.sederj" target="_blank">youTube</a></li>
+        </ul>
+      </footer>
+    </>
   );
 }
