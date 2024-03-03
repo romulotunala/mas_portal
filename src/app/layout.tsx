3f58d06be3b23448f08e41ebaa38e05903972e18
Rômulo Tunala <romulot@ciandt.com>
2024-03-03 11:51:02 -0300

    Adding Goole Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { GoogleAnalytics } from '@next/third-parties/google'
 import { Inter } from "next/font/google";
 import "./globals.css";
 // import Head from "next/head";
@@ -15,6 +16,8 @@ export const metadata: Metadata = {
   }]
 };
 
+const { NEXT_PUBLIC_GOOGLE_ANALYTICS } = process.env;
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -23,6 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <GoogleAnalytics gaId={NEXT_PUBLIC_GOOGLE_ANALYTICS as string} />
     </html>
   );
 }
